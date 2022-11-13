1d68b2b26cde5fc7c8be4203ad85b2d75c7b409a
dokim <dogdo5726@gmail.com>
2022-11-13 22:41:20 +0900

    [add] (기초문제)  4375번 - 모듈러 문제 성공

--- src/Baekjoon_CodingTestBasic/Main_1_10430_pass.java
+++ src/Baekjoon_CodingTestBasic/Main_1_10430_pass.java
@@ -1,10 +1,10 @@
-package Baekjoon;
-//https://code.plus/course/51
-//https://www.acmicpc.net/problem/1920
-//https://st-lab.tistory.com/19
+package Baekjoon_CodingTestBasic;
+// 기초 문제 : https://code.plus/course/51
+// 문제 : https://www.acmicpc.net/problem/10430
+// 참고 : https://st-lab.tistory.com/19
 import java.util.Scanner;
 
-class Main10430_pass {
+class Main_1_10430_pass {
 
     public static void main(String[] args){
 
---
+++ src/Baekjoon_CodingTestBasic/Main_3_1037.java
@@ -0,0 +1,16 @@
+package Baekjoon_CodingTestBasic;
+// 기초 문제 : https://code.plus/course/51
+// 문제 :
+// 참고 :
+
+import java.util.Scanner;
+
+class Main_3_1037 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
