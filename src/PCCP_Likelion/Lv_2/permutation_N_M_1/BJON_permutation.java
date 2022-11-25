0b450e3254d2d7bb7f980b8d1c497b2c7d85a107
dokim <dogdo5726@gmail.com>
2022-11-25 17:57:05 +0900

    [ADD] 프로그래머스 레벨 2 해결

---
+++ src/PCCP_Likelion/Lv_2/permutation_N_M_1/BJON_permutation.java
@@ -0,0 +1,33 @@
+package PCCP_Likelion.Lv_2.permutation_N_M_1;
+//https://www.acmicpc.net/problem/15649
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BJON_permutation {
+
+    public static void recursion(int m){
+        if(m == 0)
+            return;
+
+        recursion(m-1);
+        System.out.println(m);
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            recursion(i);
+        }
+
+    }
+}
+
+
