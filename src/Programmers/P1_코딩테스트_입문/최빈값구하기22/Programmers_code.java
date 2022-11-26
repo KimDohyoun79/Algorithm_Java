be857ae650bbde48955c9e5329f8a4c28e051d9b
dokim <dogdo5726@gmail.com>
2022-11-26 20:24:10 +0900

    [ADD] 프로그래머스 입문 문제

---
+++ src/Programmers/P1_코딩테스트_입문/최빈값구하기22/Programmers_code.java
@@ -0,0 +1,38 @@
+package Programmers.P1_코딩테스트_입문.최빈값구하기22;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120812
+
+
+import java.util.HashMap;
+
+public class Programmers_code {
+    public static int solution(int[] array) {
+        HashMap<Integer, Integer> arrMap = new HashMap<>();
+
+        for (int i = 0; i < array.length; i++) {
+            arrMap.put(array[i], arrMap.getOrDefault(array[i], 1)+1);
+        }
+
+        System.out.println(arrMap);
+        int maxKey = 0;
+        int maxValue = -1;
+        for (int key: arrMap.keySet()) {
+            if(maxValue < arrMap.get(key)) {
+                System.out.println(key);
+                maxValue = arrMap.get(key);
+                maxKey = key;
+            }
+        }
+
+        for (int key: arrMap.keySet()) {
+            if(maxValue == arrMap.get(key) && key!=maxKey) {
+                return -1;
+            }
+        }
+
+        return maxKey;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{1, 2, 3, 3, 3, 4})); //3
+    }
+}
