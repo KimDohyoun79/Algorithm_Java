0fbef6b0690d9273360c31328224e0be42ba9384
dokim <dogdo5726@gmail.com>
2022-11-28 11:36:34 +0900

    [ADD] 프로그래머스 입문 문제

---
+++ src/Programmers/P1_코딩테스트_입문/배열의평균값/Programmers_code.java
@@ -0,0 +1,21 @@
+package Programmers.P1_코딩테스트_입문.배열의평균값;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120813
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Programmers_code {
+    public static double solution(int[] numbers) {
+        double answer = 0;
+        for (int num : numbers){
+            answer += num;
+        }
+        return answer/numbers.length;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    }
+}
