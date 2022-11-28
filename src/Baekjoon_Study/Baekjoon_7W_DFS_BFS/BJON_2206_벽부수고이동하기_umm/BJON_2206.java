ebb1fe2a9235b7c48038e99cc9fc41768c52712e
dokim <dogdo5726@gmail.com>
2022-11-28 18:11:25 +0900

    [ADD] 백준 스터디 DFS 문제 성공

--- src/Baekjoon_Study/Baekjoon_7W_DFS_BFS/BJON_2206_벽부수고이동하기_umm/BJON_2206.java
+++ src/Baekjoon_Study/Baekjoon_7W_DFS_BFS/BJON_2206_벽부수고이동하기_umm/BJON_2206.java
@@ -1,8 +1,7 @@
-package Baekjoon_Study.Baekjoon_7W_DFS_BFS.BJON_2206_벽부수고이동하기;
+package Baekjoon_Study.Baekjoon_7W_DFS_BFS.BJON_2206_벽부수고이동하기_umm;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 class BJON_2206 {
