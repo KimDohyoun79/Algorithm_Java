59e228be2dc24073ae316eeb9b7865c6ece8b1b2
dokim <dogdo5726@gmail.com>
2022-11-30 00:51:24 +0900

    [ADD] 프로그래머스 입문 문제 성공

--- src/Programmers/P1_코딩테스트_입문/숫자찾기/Programmers_code.java
+++ src/Programmers/P1_코딩테스트_입문/숫자찾기/Programmers_code.java
@@ -3,7 +3,17 @@ package Programmers.P1_코딩테스트_입문.숫자찾기;
 
 
 public class Programmers_code {
+
     public static int solution(int num, int k) {
+
+        String s = String.valueOf(num);
+        String sk = String.valueOf(k);
+
+        return s.indexOf(sk)<0? -1:s.indexOf(sk)+1;
+    }
+
+
+    public static int solution1(int num, int k) {
 
         String s = String.valueOf(num);
         for (int i = 0; i < s.length(); i++) {
