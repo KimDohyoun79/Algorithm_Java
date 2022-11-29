ecd74f4fd6192af684a74933a2e519325f2f2b04
dokim <dogdo5726@gmail.com>
2022-11-30 00:25:51 +0900

    [ADD] 프로그래머스 입문 문제 성공

--- src/Programmers/P1_코딩테스트_입문/옷가게할인받기/Programmers_code.java
+++ src/Programmers/P1_코딩테스트_입문/옷가게할인받기/Programmers_code.java
@@ -5,19 +5,21 @@ package Programmers.P1_코딩테스트_입문.옷가게할인받기;
 public class Programmers_code {
     public static int solution(int price) {
 
-        int discount = 1;
+        int discount = 0;
         if (100_000 <= price && price < 300_000)
             discount = 5;
         else if (300_000 <= price && price < 500_000)
             discount = 10;
         else if (500_000 <= price)
             discount = 20;
-        return price - (price * discount / 100);
+        return price*100 - (price * discount / 100);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(solution(150_000));
-        System.out.println(solution(580_000));
+        System.out.println(solution(100_000)); // 95,000
+        System.out.println(solution(150_000)); // 142,500
+        System.out.println(solution(580_000)); // 464,000
+        System.out.println(solution(1_000_000)); // 464,000
     }
 }
