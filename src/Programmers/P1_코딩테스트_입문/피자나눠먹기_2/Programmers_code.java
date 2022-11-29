6833421646bdbdac39961c64d7af7ac754e94155
dokim <dogdo5726@gmail.com>
2022-11-29 18:07:08 +0900

    [ADD] 프로그래머스 입문 문제 성공

---
+++ src/Programmers/P1_코딩테스트_입문/피자나눠먹기_2/Programmers_code.java
@@ -0,0 +1,25 @@
+package Programmers.P1_코딩테스트_입문.피자나눠먹기_2;
+// https://school.programmers.co.kr/learn/courses/30/lessons/120812
+
+
+public class Programmers_code {
+
+    public static int solution(int n) {
+
+        int i = 1;
+        int pizza = 6;
+        while (pizza % n != 0) {
+            i++;
+            pizza = 6 * i;
+        }
+        return i;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(6)); // 1
+        System.out.println(solution(10)); // 5
+        System.out.println(solution(4)); // 2
+    }
+}
