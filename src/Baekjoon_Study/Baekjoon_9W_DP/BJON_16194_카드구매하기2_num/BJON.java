896b7a6a9ebb2c0c040e2d77235e12be3ac927b6
dokim <dogdo5726@gmail.com>
2022-12-06 16:44:42 +0900

    [ADD] 프로그래머스 문제 성공

---
+++ src/Baekjoon_Study/Baekjoon_9W_DP/BJON_16194_카드구매하기2_num/BJON.java
@@ -0,0 +1,17 @@
+package Baekjoon_Study.Baekjoon_9W_DP.BJON_16194_카드구매하기2_num;
+
+import java.util.Scanner;
+
+class BJON {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
