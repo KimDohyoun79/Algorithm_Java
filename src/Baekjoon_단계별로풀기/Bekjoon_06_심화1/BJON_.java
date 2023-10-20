ca08e393fd781eea9255cbbf28db33a67e5c169d
KimDohyoun79 <dogdo5726@gmail.com>
2023-10-21 01:01:24 +0900

    [feat] 백준 25083 새싹 문제 해결
    - BufferedWriter 사용하여 해결

---
+++ src/Baekjoon_단계별로풀기/Bekjoon_06_심화1/BJON_.java
@@ -0,0 +1,10 @@
+package Baekjoon_단계별로풀기.Bekjoon_06_심화1;
+//https://www.acmicpc.net/problem/1546
+
+public class BJON_ {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Baekjoon_단계별로풀기/Bekjoon_06_심화1/BJON_25083.java
@@ -0,0 +1,22 @@
+package Baekjoon_단계별로풀기.Bekjoon_06_심화1;
+//https://www.acmicpc.net/problem/25083
+
+import java.io.*;
+
+public class BJON_25083 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("         ,r'\"7\n");
+        bw.write("r`-_   ,'  ,/\n");
+        bw.write(" \\. \". L_r'\n");
+        bw.write("   `~\\/\n");
+        bw.write("      |\n");
+        bw.write("      |\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
