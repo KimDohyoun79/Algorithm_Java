31355f2fe89aa6021ff6e62baa1880680d97c0a1
KimDohyoun79 <dogdo5726@gmail.com>
2023-10-21 01:36:36 +0900

    [feat] 백준 2941 크로아티아알파벳 문제 해결

---
+++ src/Baekjoon_단계별로풀기/Bekjoon_06_심화1/크로아티아알파벳/BJON_2941.java
@@ -0,0 +1,37 @@
+package Baekjoon_단계별로풀기.Bekjoon_06_심화1.크로아티아알파벳;
+//https://www.acmicpc.net/problem/2941
+
+import java.io.*;
+
+public class BJON_2941 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String str = bf.readLine();
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        int size = str.length();
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(str.contains(arr[i])){
+                str = str.replaceFirst(arr[i], "1");
+                size = str.length();
+                count++;
+                i--;
+            }
+        }
+
+        for(int i = 0; i < str.length(); i++){
+            if('a' <= str.charAt(i) && str.charAt(i) <= 'z'){
+                count++;
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+
+    }
+}
