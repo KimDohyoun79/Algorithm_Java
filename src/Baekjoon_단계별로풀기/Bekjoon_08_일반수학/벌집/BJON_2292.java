981c647a4a733c38bcd655bfda3ca93cbaac0fd6
KimDohyoun79 <dogdo5726@gmail.com>
2023-10-24 18:22:03 +0900

    [feat] 백준 문제 해결

---
+++ src/Baekjoon_단계별로풀기/Bekjoon_08_일반수학/벌집/BJON_2292.java
@@ -0,0 +1,26 @@
+package Baekjoon_단계별로풀기.Bekjoon_08_일반수학.벌집;
+//https://www.acmicpc.net/problem/2292
+
+import java.io.*;
+
+public class BJON_2292 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+
+        int count = 1;
+        int i = 1;
+        if(n != 1){
+            while(n > i){
+                i = i + (6 * count);
+                count++;
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(count));
+        bw.close();
+    }
+}
