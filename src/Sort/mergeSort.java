4012756df0930c05c383d1dc1e66b885d8baf6c3
KimDohyoun79 <dogdo5726@gmail.com>
2023-11-30 16:04:30 +0900

    [feat] 퀵정렬 구현

---
+++ src/Sort/mergeSort.java
@@ -0,0 +1,17 @@
+package Sort;
+
+import java.util.Scanner;
+
+public class mergeSort {
+
+
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+
---
+++ src/Sort/quickSort.java
@@ -0,0 +1,52 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class quickSort {
+
+    public static void QuickSort(int[] arr){
+        QuickSort(arr, 0, arr.length - 1);
+    }
+
+    // 분할
+    public static void QuickSort(int[] arr, int start, int end){
+        int pivot = partition(arr, start, end);
+        if(start < pivot - 1)
+            QuickSort(arr, start, pivot - 1);
+        if(pivot < end)
+            QuickSort(arr, pivot, end);
+    }
+
+    // 정복
+    public static int partition(int[] arr, int start, int end){
+        int pivot = arr[(start + end) / 2];
+
+        while(start <= end){
+            while(arr[start] < pivot) start++;
+            while(pivot < arr[end]) end--;
+            if(start <= end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
+    public static void swap(int[] arr, int start, int end){
+        int tmp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = tmp;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1, 9, 8, 7, 6};
+        QuickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+
